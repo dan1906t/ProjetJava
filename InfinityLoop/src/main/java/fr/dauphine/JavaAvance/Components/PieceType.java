829d3c3e241784f5b0f5a6080d8bbfa2bfacb507
Yariv1712 <57892762+Yariv1712@users.noreply.github.com>
2022-01-08 23:08:42 +0100

    Add files via upload

---
+++ InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
@@ -0,0 +1,20 @@
+package fr.dauphine.JavaAvance.Components;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * 
+ * Type of the piece enum
+ * 
+ */
+public enum PieceType {
+	// Each Type has a number of connectors and a specific value
+	VOID;
+	
+	public Orientation getOrientation(Orientation o) {return o;};
+	
+	public void setConnectorsList(Orientation O) {};
+	
+}
